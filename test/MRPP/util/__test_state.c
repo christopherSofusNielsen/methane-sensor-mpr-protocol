8d3e3a66f04f2171a0cd450cb947b863609ea3e2
Christopher Nielsen <c.consult.christopher@gmail.com>
2021-09-21 21:06:08 +0200

    added extra test

--- test/MRPP/util/__test_state.c
+++ test/MRPP/util/__test_state.c
@@ -66,6 +66,40 @@ void test_init_2(){
     TEST_ASSERT_EQUAL_UINT8(WAITING, state.collections[0].status);
 }
 
+void test_init_3(){
+    MRPP_STATE state;
+    COLLECTION cols[2]={
+        {.samplings=20, .type=T_INT16, .samplingInterval=15},
+        {.samplings=42, .type=T_INT8, .samplingInterval=15}
+    };
+    mrpp_state_init(&state, 1, cols, 2);
+
+    TEST_ASSERT_EQUAL_UINT8(1, state.groupId);
+    TEST_ASSERT_EQUAL_UINT8(3, state.lastSubId);
+    TEST_ASSERT_EQUAL_UINT8(2, state.nCollections);
+    TEST_ASSERT_EQUAL_UINT8(2, state.nBodies);
+
+    //Test bodies
+    TEST_ASSERT_EACH_EQUAL_UINT8(WAITING, state.bodies, 2);
+   
+    //Test collections
+    TEST_ASSERT_EQUAL_UINT16(0, state.collections[0].startIndex);
+    TEST_ASSERT_EQUAL_UINT16(47, state.collections[0].length);
+    TEST_ASSERT_EQUAL_UINT8(T_INT16, state.collections[0].type);
+    TEST_ASSERT_EQUAL_UINT16(15, state.collections[0].samplingInterval);
+    TEST_ASSERT_EQUAL_UINT8(0, state.collections[0].beginsInBody);
+    TEST_ASSERT_EQUAL_UINT8(0, state.collections[0].endsInBody);
+    TEST_ASSERT_EQUAL_UINT8(WAITING, state.collections[0].status);
+
+    TEST_ASSERT_EQUAL_UINT16(47, state.collections[1].startIndex);
+    TEST_ASSERT_EQUAL_UINT16(49, state.collections[1].length);
+    TEST_ASSERT_EQUAL_UINT8(T_INT8, state.collections[1].type);
+    TEST_ASSERT_EQUAL_UINT16(15, state.collections[1].samplingInterval);
+    TEST_ASSERT_EQUAL_UINT8(0, state.collections[1].beginsInBody);
+    TEST_ASSERT_EQUAL_UINT8(1, state.collections[1].endsInBody);
+    TEST_ASSERT_EQUAL_UINT8(WAITING, state.collections[1].status);
+}
+
 void test_get_header(){
     uint8_t test_package[]={
         0x00, //subId=0 
@@ -459,6 +493,7 @@ int main(void){
     UNITY_BEGIN();
     RUN_TEST(test_init_1);
     RUN_TEST(test_init_2);
+    RUN_TEST(test_init_3);
     RUN_TEST(test_get_header);
     RUN_TEST(test_get_tail);
     RUN_TEST(test_set_collection_1);
