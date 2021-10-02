501ab7038914bb8fbe43de50e8a72e5cbac8aeba
Christopher Nielsen <c.consult.christopher@gmail.com>
2021-10-02 11:08:05 +0200

    added integration test

---
+++ integration-test/t0.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "util/util.h"
+
+#include "../src/MRPP/mrpp.h"
+#include "../src/MRPP/util/mrpp_state.h"
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello world");
+    return 0;
+}
